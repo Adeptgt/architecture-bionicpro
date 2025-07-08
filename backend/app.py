8b679870d96dd5bc3fa1657617bbfc2159b449dd
Valeev Karim <Adeptgt@yandex.ru>
2025-07-08 20:03:52 +0300

    fix problems

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,15 @@
 
 from fastapi import FastAPI,Request,HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-import requests
+from keycloak import KeycloakOpenID
 import random
 
+
+# Configure client
+keycloak_openid = KeycloakOpenID(server_url="http://keycloak:8080/",
+                                 client_id="reports-api",
+                                 realm_name="reports-realm",
+                                 client_secret_key="oNwoLQdvJAvRcL89SydqCWCe5ry1jMgq")
 
 
 app = FastAPI()
@@ -23,45 +29,35 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def generate(n):
+   values = ['value1', 'value2']
+   mydict = {"key " + str(i): values[0] for i in range(n)}
+   mydict["key " + str(random.randrange(n))] = values[1]
+   return mydict
 
-def get_user_info(token):
-   session = requests.Session()
-   session.headers.update({
-    'Authorization': f'{token}',
-   'Accept-Language': 'en-US',
-   'Content-Type': 'application/json'
-   })
-   try:
-       response=requests.get('http://keycloak:8080/realms/reports-realm/protocol/openid-connect/userinfo',headers=headers)
-       if response.status_code != 200:
-                raise HTTPException(
-                    status_code=401, detail=f"Invalid access token: {response} headers: {headers}"
-                )
-       return response.json()
-   except ConnectionError  as e:
-            raise HTTPException(
-                status_code=500, detail=f"Keycloak request error: {str(e)}"
-        )
-       
 
 
 @app.get("/")
 def read_root(request: Request,  status_code=200):
-    user_info=get_user_info(request.headers.get("authorization"))
+    config_well_known = keycloak_openid.well_known()
    
-    return {'authorization':user_info}
+    return {'config_well_known':config_well_known}
 
 @app.get("/reports",  status_code=200)
 def reports(request: Request):
- #   user_info=get_user_info(request.headers.get("authorization")) 
+    tmp=request.headers.get("authorization")
+    token=str.split(tmp,' ')
+    try: 
+     token_info = keycloak_openid.decode_token(token[1])
+    except Exception as e:
+        raise HTTPException(status_code=401, detail="Не валидный токен!")
+    
 
-    user_info=dict() #  Заглушка для отладки логики.
-    user_info['roles']=['prothetic_user','test'] #
+    if 'prothetic_user' in token_info['realm_access']['roles'] :
+      
 
-    if 'prothetic_user' in user_info['roles']:
-      report = [[random.randrange(1,1000,1)] * 100 for i in range(1000)]
-      return {"report": report}
+      return generate(100)
     else:
-       raise HTTPException(status_code=401, detail="you don't have access")
+       raise HTTPException(status_code=401, detail="У вас нет доступа!")
     
     
